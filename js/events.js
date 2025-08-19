7e12cce795a038adc3358c45a0b23c39583c4b82
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-08-20 03:19:39 +0600

    dom_events_handler

--- js/events.js
+++ js/events.js
@@ -1,1 +1,25 @@
-console.log('External File');
+//  option 2 click event handler
+
+function changeBg() {
+    document.body.style.backgroundColor = 'lightgreen';
+}
+
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+// option 3 get element by id or class then set onClick
+
+const blueBtn = document.getElementById('btn-blue');
+    // console.log(blueBtn);
+    blueBtn.onclick = function makeBlue() {
+    document.body.style.backgroundColor = 'pink';}
+
+
+// option 3 slight different version
+
+const purpleBtn = document.getElementById('btn-purple');
+    purpleBtn.onclick = makePurpleBtn;
+    function makePurpleBtn() {
+    document.body.style.backgroundColor = 'purple';
+    }
